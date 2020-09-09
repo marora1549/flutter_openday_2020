4c9ab35a1698578470f2807f05db30606ddd5c2f
Mohan Arora <marora1549@gmail.com>
2020-09-09 20:01:42 +0530

    om

---
+++ lib/homePage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/images/slides1.png'),
+          fit: BoxFit.fill,
+        )),
+      ),
+    );
+  }
+}
